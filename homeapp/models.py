0602c7c3924766a3bffef3d103bf3a35b7c6f9b4
Michael-oss138 <nwaizumichael0@gmail.com>
2025-08-26 21:36:39 +0100

    ready to deploy

--- homeapp/models.py
+++ homeapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 # Create your models here.
+#"3050755494"
 class Subscribe(models.Model):
     email = models.EmailField(unique=True)
     subscibed_at = models.DateTimeField(auto_now_add=True)
--- homeproject/settings.py
+++ homeproject/settings.py
@@ -22,16 +22,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-m*!&%3^zcbn8w99mcpn&335s3pkue=oi43t$&99=us@ic-tn%q'
+SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -42,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -122,6 +125,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
